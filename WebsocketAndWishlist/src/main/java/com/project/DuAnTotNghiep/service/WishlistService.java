0c335db4ea1eaa5611b0177c3f069f87e9d5bfa0
ndc87 <duyc080807@gmail.com>
2025-10-22 14:47:59 +0700

    xóa bỏ phần wishlist

--- WebsocketAndWishlist/src/main/java/com/project/DuAnTotNghiep/service/WishlistService.java
+++
@@ -1,9 +0,0 @@
-package com.project.DuAnTotNghiep.service;
-
-import com.project.DuAnTotNghiep.entity.WishlistItemEntity;
-import java.util.List;
-
-public interface WishlistService {
-    List<WishlistItemEntity> getWishlistItems(Long accountId);
-    int countWishlistItems(Long accountId);
-}

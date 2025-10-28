a2c8b449a5ae525ac971fae715dc78d792036361
ndc87 <duyc080807@gmail.com>
2025-10-28 18:07:59 +0700

    d

--- VENDORUPDATE/src/main/java/com/project/DuAnTotNghiep/controller/vendor/VendorProductViewController.java
+++ VENDORUPDATE/src/main/java/com/project/DuAnTotNghiep/controller/vendor/VendorProductViewController.java
@@ -2,13 +2,16 @@ package com.project.DuAnTotNghiep.controller.vendor;
 
 import com.project.DuAnTotNghiep.entity.Branch;
 import com.project.DuAnTotNghiep.repository.BranchRepository;
+import com.project.DuAnTotNghiep.utils.UserLoginUtil;
 import com.project.DuAnTotNghiep.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/vendor-page")
@@ -51,5 +54,15 @@ public class VendorProductViewController {
 
         model.addAttribute("branch", branch);
         return "vendor/product-detail";
+    }
+    @GetMapping("/api/vendor/branch-id")
+    @ResponseBody
+    public ResponseEntity<Long> getCurrentVendorBranchId(Authentication authentication) {
+        if (authentication != null && authentication.getPrincipal() instanceof com.project.DuAnTotNghiep.security.CustomUserDetails userDetails) {
+            if (userDetails.getAccount() != null && userDetails.getAccount().getBranch() != null) {
+                return ResponseEntity.ok(userDetails.getAccount().getBranch().getId());
+            }
+        }
+        return ResponseEntity.ok(null);
     }
 }
